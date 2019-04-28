1abee937a0cfdb75886350b450c1ed71e6d3ecac
Patrick A <akolo@hotmail.com>
2019-04-28 11:55:36 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,3 @@
-# require libraries/modules here
 require "nokogiri"
 
 def create_project_hash
@@ -8,12 +7,7 @@ def create_project_hash
     projects = {}
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text 
-      projects[title] = {
-        :image_link => project.css ("div.project-thumbnail a img").attribute ("src").value,
-        :description => project.css("p.bbcard_blurb").text, 
-        :location => project.css("ul.project-meta span.location-name").text,
-        :percent_funded => project("ul.project-stats li.first.funded strong").text.gsub("%", " ").to_i
-      }
+      projects[title] = {:image_link => project.css ("div.project-thumbnail a img").attribute ("src").value, :description => project.css("p.bbcard_blurb").text, :location => project.css("ul.project-meta span.location-name").text, :percent_funded => project("ul.project-stats li.first.funded strong").text.gsub("%", " ").to_i}
     end 
     projects
 end
